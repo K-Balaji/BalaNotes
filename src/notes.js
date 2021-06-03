bcd20e5f7851cb724ff7d620990ecf6224632ba0
Balaji K <balajikyadav.official@gmail.com>
2021-06-03 14:25:03 +0530

    Improved CSS

--- src/notes.js
+++ src/notes.js
@@ -5,7 +5,7 @@ var themeSVG = document.getElementById("theme");
 var search = document.getElementById("search_text");
 var themeChange = document.getElementById("themeChange");
 function generateCard(element0, element1, index) {
-    var card = "<div\n      class=\"\n        my-3\n        mx-3\n        border-0\n        rounded-xl\n        bg-gradient-to-b\n        box\n        from-blue-500\n        to-green-500\n        dark:from-blue-600\n        dark:to-pink-600\n      \"\n      style=\"width: 20rem\"\n    >\n      <div class=\"p-3 text-body\">\n        <div>\n          <h5 class=\"dark:text-blue-200 font-bold\">" + element0 + "</h5>\n          <p class=\"dark:text-blue-200\">" + element1 + "</p>\n        </div>\n      </div>\n      <div class=\"p-3\">\n        <button\n          class=\"\n            text-base\n            hover:shadow-lg\n            hover:bg-purple-600\n            bg-purple-700\n            p-2\n            rounded-lg\n            text-gray-200\n          \"\n          onclick=\"dele(" + index + ")\"\n        >\n          Delete\n        </button>\n      </div>\n    </div>";
+    var card = "<div\n      class=\"\n        my-3\n        mx-3\n        border-0\n        rounded-xl\n        bg-gradient-to-b\n        box\n        from-blue-500\n        to-green-500\n        dark:from-blue-600\n        dark:to-pink-600\n      \"\n      style=\"width: 20rem\"\n    >\n      <div class=\"p-3 text-body\">\n        <div>\n          <h5 class=\"dark:text-blue-200 font-bold\">" + element0 + "</h5>\n          <p class=\"dark:text-blue-200\">" + element1 + "</p>\n        </div>\n      </div>\n      <div class=\"p-3 flex justify-end\">\n        <button\n          class=\"\n            text-base\n            hover:shadow-lg\n            hover:bg-purple-600\n            bg-purple-700\n            p-2\n            rounded-lg\n            text-gray-200\n          \"\n          onclick=\"dele(" + index + ")\"\n        >\n          Delete\n        </button>\n      </div>\n    </div>";
     return card;
 }
 function updateTheme() {
@@ -14,20 +14,17 @@ function updateTheme() {
         document.getElementById("html").setAttribute("class", "dark");
         themeSVG.innerHTML =
             '<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />';
-        themeSVG.setAttribute("class", "h-4 w-4");
     }
     else if (theme === null) {
         localStorage.setItem("notes_theme", JSON.stringify("light"));
         themeSVG.innerHTML =
             '<path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />';
         document.getElementById("html").setAttribute("class", "");
-        themeSVG.setAttribute("class", "h-5 w-5");
     }
     else if (theme == "light") {
         document.getElementById("html").setAttribute("class", "");
         themeSVG.innerHTML =
             '<path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />';
-        themeSVG.setAttribute("class", "h-5 w-5");
     }
     else {
         throw Error("unrecognizedThemeValue");
